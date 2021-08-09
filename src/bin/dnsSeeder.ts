7ba89f196cd24692fe02d9522a9e11828d8959fe
Boone Ferry <boone.ferry@dexcarehealth.com>
2021-08-09 00:07:06 -0700

    fix: add commands

--- src/bin/dnsSeeder.ts
+++ src/bin/dnsSeeder.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { setupCommand } from 'src/commands/setup';
+import { startCommand } from 'src/commands/start';
 import updateNotifier from 'update-notifier';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -10,6 +12,7 @@ updateNotifier({
     updateCheckInterval: 0,
 }).notify();
 
+/* DNS Seeder Command Init */
 const dnsSeeder = new Command();
 dnsSeeder
     .version(pkg.version)
@@ -20,6 +23,11 @@ dnsSeeder
         dnsSeeder.outputHelp();
     });
 
+/* Commands */
+dnsSeeder.addCommand(setupCommand);
+dnsSeeder.addCommand(startCommand);
+
+/* Parse Options */
 dnsSeeder.parse(process.argv);
 const options = dnsSeeder.opts();
 if (options.apiToken) process.env.CLOUDFLARE_TOKEN = options.apiToken;
