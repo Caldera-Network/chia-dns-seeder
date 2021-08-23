30ab07201ef12b552dafd75bb1821223c791b892
Boone Ferry <boone.ferry@dexcarehealth.com>
2021-08-23 00:51:47 -0700

    fix: deprecate cloudflare npm package

--- src/lib/cloudflare/cloudflare.types.ts
+++ src/lib/cloudflare/cloudflare.types.ts
@@ -10,3 +10,61 @@ export interface CloudFlareResults {
     ttl: number;
     locked: boolean;
 }
+
+type RecordTypes =
+    | 'A'
+    | 'AAAA'
+    | 'CNAME'
+    | 'HTTPS'
+    | 'TXT'
+    | 'SRV'
+    | 'LOC'
+    | 'MX'
+    | 'NS'
+    | 'SPF'
+    | 'CERT'
+    | 'DNSKEY'
+    | 'DS'
+    | 'NAPTR'
+    | 'SMIMEA'
+    | 'SSHFP'
+    | 'SVCB'
+    | 'TLSA'
+    | 'URI read only';
+
+interface DnsRecordWithoutPriority {
+    type: Exclude<RecordTypes, 'MX' | 'SRV' | 'URI'>;
+    name: string;
+    content: string;
+    ttl: number;
+    proxied?: boolean | undefined;
+}
+
+interface DnsRecordWithPriority {
+    type: Exclude<RecordTypes, 'MX' | 'SRV' | 'URI'>;
+    name: string;
+    content: string;
+    ttl: number;
+    proxied?: boolean | undefined;
+    priority: number;
+}
+
+interface SrvDnsRecord {
+    type: 'SRV';
+    data: {
+        name: string;
+        service: string;
+        proto: string;
+        ttl: number;
+        proxied?: boolean | undefined;
+        priority: number;
+        weight: number;
+        port: number;
+        target: string;
+    };
+}
+
+export type DnsRecord =
+    | DnsRecordWithPriority
+    | DnsRecordWithoutPriority
+    | SrvDnsRecord;
